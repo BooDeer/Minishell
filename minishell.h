b0ee47935b4260071d8edd2ff835b8f61542398a
BooDeer <hsissi3a@gmail.com>
2020-12-28 17:35:56 +0100

    Added some libraries

--- minishell.h
+++ minishell.h
@@ -4,10 +4,16 @@
 # include <stdio.h>
 # include <unistd.h>
 # include <stdlib.h>
-
-
-
-
+# include <fcntl.h>
+# include <errno.h>
+# include <string.h>
+# include <dirent.h>
+# include <signal.h>
+# include <sys/resource.h>
+# include <sys/time.h>
+# include <sys/stat.h>
+# include <sys/wait.h>
+# include <sys/types.h>
 
 #endif
 
