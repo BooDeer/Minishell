347ac4676ae60e18dc99fbb430c171f92ae74663
BooDeer <hsissi3a@gmail.com>
2021-02-02 16:19:14 +0100

    Fixed segfault

--- main.c
+++ main.c
@@ -2,17 +2,4 @@
 
 int	main(void)
 {
-<<<<<<< HEAD
-		//int		fd = open("main.c", O_RDONLY);
-		//printf("Current value of fd: %d\n", fd);
-		//close(fd);
-		//printf("Current value of fd: %d\n", fd);
-		//char *buffer = NULL;
-		//read(fd, buffer, 10);
-		//printf("The buffer's content: %s\n", buffer);
-
-	printf("Pepepopo") && printf("Pepepopo") || printf("Pepepopo");
-=======
-	pepepepopo
->>>>>>> 88945adf859911e8abf07d0872281113b4b8075e
 }
