8de0fef8a9dfb9521482067cd776bb67eed2c8f6
BooDeer <hsissi3a@gmail.com>
2021-05-18 15:41:46 +0100

    to work

--- main.c
+++ main.c
@@ -35,7 +35,7 @@ char **g_env;
 //		return (ft_builtin_exit(cmd + 1));
 
 //}
-/*
+
 int		exit_program(char *error_msg, int fd, int exit_code)
 {
 	write(fd, error_msg, ft_strlen(error_msg));
@@ -119,6 +119,13 @@ char		*ft_pathjoin(char  *s1, char *s2)
 			return (tmp);
 		}
 	}
+	else
+	{
+		if (s2[0] == '/')
+			return (ft_strjoin(s1, s2 + 1));
+		else
+			return (ft_strjoin(s1, s2));
+	}
 }
 
 int			run_cmd(char *exec_path, char **args)
@@ -126,6 +133,7 @@ int			run_cmd(char *exec_path, char **args)
 	pid_t	pid;
 
 	pid = fork();
+	printf("\n%s\n", exec_path);
 	if (pid == 0)
 		execve(exec_path, args, g_env);
 	else if (pid < 0)
@@ -134,6 +142,10 @@ int			run_cmd(char *exec_path, char **args)
 		write(1, "Failed to create fork\n", ft_strlen("Failed to create fork\n"));
 		return (-1);
 	}
+	wait(&pid);
+	if (exec_path)
+		free(exec_path);
+	return (1);
 }
 
 int			is_exec(char *exec_path, struct stat stats, char **cmd)
@@ -157,15 +169,14 @@ int			exec_cmd(char **cmd)
 	int		i;
 	char	*exec_path;
 	struct stat stats;
+
 	i = -1;
 	while (path && path[++i])
 	{
 		if (ft_strstartw(cmd[0], path[i]))
-		{
 			exec_path = ft_strdup(cmd[0]);
-		}
 		else
-			exec_path = ft_strjoin(path[i], cmd[0]);
+			exec_path = ft_pathjoin(path[i], cmd[0]);
 		if (lstat(exec_path, &stats) == -1)
 			free(exec_path);
 		else
@@ -174,6 +185,8 @@ int			exec_cmd(char **cmd)
 			return (is_exec(exec_path, stats, cmd));
 		}
 	}
+	ft_freestrarr(path);
+	return (0);
 	//int		pid;
 
 	//pid = fork();
@@ -192,7 +205,6 @@ int			exec_cmd(char **cmd)
 
 }
 
-*/
 
 void		*safe_malloc(size_t size)
 {
@@ -221,15 +233,24 @@ void		init_env(int argc, char **argv, char **env)
 	(void)argc;
 	(void)argv;
 
-	g_env = (char**)safe_malloc(sizeof(char *) *env_len(env) + 1)
+	g_env = (char**)safe_malloc(sizeof(char *) * (env_len(env) + 1));
+	i = -1;
+	while (env[++i])
+	{
+		if (!(g_env[i] = ft_strdup(env[i])))
+			exit_program("A memory allocation failed!\n", 2, 0);
+	}
 }
 
 int			main(int argc, char **argv, char **env)
 {
 
 	init_env(argc, argv, env);
-	char	*cmd[] = {"ls", "-C", NULL};
+	char	*cmd[] = {"ping", "google.com", NULL};
 	exec_cmd(cmd);
+	//int	i = -1;
+	//while (g_env[++i])
+	//	printf("%s\n", g_env[i]);
 	//char **path = ft_split(getenv("PATH"), ':');
 	//int i = -1;
 	//printf("===================\n===================\n");
